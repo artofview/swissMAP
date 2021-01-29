11b9a351e9de50434fd246abc0a2c6dd8a3f5f85
Markus Grütter <markus@artofview.ch>
2021-01-30 00:21:19 +0100

    init

---
+++ shp2topo.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var shapefile = require('shapefile');
+var topojson = require('topojson-server');
+
+// specify the output file here
+const inputFile = 'g2g20.shp'
+const outputFile = 'geo.json'
+
+shapefile
+  .open(inputFile)
+  .then(source => {
+    // start with empty geojson skeleton
+    let geojson = {
+      type: 'FeatureCollection',
+      features: [] // into this array we push our features below
+    }
+    return source.read().then(function log (result) {
+      // when done: pass geojson to next function in promise pipeline
+      if (result.done) return geojson
+
+      // if not done: add to geojson feature by feature
+      const feature = {
+        //destructuring assignment : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+        ...result.value,
+        // keep from properties only the bfs id
+        properties: {
+          bfs_id: result.value.properties.GMDNR
+        }
+      }
+      geojson.features.push(feature)
+      // continue with next feature/iteration
+      return source.read().then(log)
+    })
+  })
+  // just before write to file add the following line:
+  .then(geojson => 
+    //topojson.topology({ municipalities: geojson })
+    topojson.topology({ municipalities: geojson }, 1000)      //1000 : Quantization -> die Koordinaten werden als Punkte eines definierten Grids interpretiert und NICHT als beliebiger Float
+  )
+  // write to file
+  .then(fileContent =>
+    fs.writeFile(outputFile, JSON.stringify(fileContent), () => { 
+      console.log('The file has been saved!')
+    })
+  )
+  .catch(console.error)
